c6cbece84d742d8c5edb061cd5280572dd31d61a
sportsfanatic1010 <79819657+sportsfanatic1010@users.noreply.github.com>
2024-02-06 21:25:05 -0800

    Updated code to use the Library of Congress API https://www.loc.gov/apis/ Using JSON API Program remains nonfunctional--Unable to use API properly currently
    Version 1.1 (21:26, Tuesday, February 6, 2024)
    As most libraries (physical libraries) do not have API's for their websites, the program will utilize web scraping for them

--- Library Catalog.py
+++ Library Catalog.py
@@ -4,8 +4,23 @@ import requests
 from bs4 import BeautifulSoup
 
 # Example URL for the Library of Congress: https://catalog.loc.gov/vwebv/search?searchArg=SEARCHARGUMENTGOESHERE&searchCode=GKEY%5E*&searchType=0&recCount=25
+endpoint = 'books'
+url = f'https://www.loc.gov/{endpoint}/?fo=json'
+PARAMETERS = {
+    'q' : "all the light we cannot see",
+    'format' : 'books',
+    'fa=language' : 'english',
+    'fa=subject' : 'Saint-Malo',
+    'sp' : '1'
+}
+request = requests.get(url=url,params = PARAMETERS)
+data = request.json()
+print()
+print()
+print(data)
 
-URL = "https://catalog.loc.gov/vwebv/search?searchArg=a+man+called+ove&searchCode=GKEY%5E*&searchType=0&recCount=25"
+
+"""URL = "https://catalog.loc.gov/vwebv/search?searchArg=a+man+called+ove&searchCode=GKEY%5E*&searchType=0&recCount=25"
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"} 
 request = requests.get(URL, headers=headers)
 soup = BeautifulSoup(request.content, 'html5lib')
@@ -21,7 +36,7 @@ for item in location_a:
     location = str(item)
     print(location)
     print(len(location_a))
-"""for x in item_a:
+for x in item_a:
     print(item)
 for item in date:
     print(date)"""
